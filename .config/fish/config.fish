76fdc6ebdf3f25688937eb81c6eb075694393483
Miella Selene Engel <justsweetluna@gmail.com>
2026-02-21 16:25:26 +0100

    auwu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,3 +28,9 @@ source ~/.cache/wal/colors.fish
 
 
 starship init fish | source
+# pnpm
+set -gx PNPM_HOME "/home/miella/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
